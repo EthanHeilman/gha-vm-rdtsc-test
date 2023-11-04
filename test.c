7000ef4a2127f525ec041a2bc08f4cc96ce93c26
Ethan Heilman <ethan.r.heilman@gmail.com>
2023-11-04 17:51:01 -0400

    Checks if gha supports rdseed rdrand

--- test.c
+++ test.c
@@ -22,6 +22,75 @@ static __inline__ unsigned long long rdtsc(void)
 
 #endif
 
+
+// Borrowed and slightly modifed from bitcoin's RNG
+// https://github.com/bitcoin/bitcoin/blob/d9007f51a7480246abe4c16f2e3d190988470bec/src/random.cpp#L109
+static unsigned long long  GetRdRand()
+{
+    // RdRand may very rarely fail. Invoke it up to 10 times in a loop to reduce this risk.
+#ifdef __i386__
+    unsigned char ok;
+    // Initialize to 0 to silence a compiler warning that r1 or r2 may be used
+    // uninitialized. Even if rdrand fails (!ok) it will set the output to 0,
+    // but there is no way that the compiler could know that.
+    unsigned integer r1 = 0, r2 = 0;
+    for (int i = 0; i < 10; ++i) {
+        __asm__ volatile (".byte 0x0f, 0xc7, 0xf0; setc %1" : "=a"(r1), "=q"(ok) :: "cc"); // rdrand %eax
+        if (ok) break;
+    }
+    for (int i = 0; i < 10; ++i) {
+        __asm__ volatile (".byte 0x0f, 0xc7, 0xf0; setc %1" : "=a"(r2), "=q"(ok) :: "cc"); // rdrand %eax
+        if (ok) break;
+    }
+    return (((uint64_t)r2) << 32) | r1;
+#elif defined(__x86_64__) || defined(__amd64__)
+    unsigned char ok;
+    unsigned long long  r1 = 0; // See above why we initialize to 0.
+    for (int i = 0; i < 10; ++i) {
+        __asm__ volatile (".byte 0x48, 0x0f, 0xc7, 0xf0; setc %1" : "=a"(r1), "=q"(ok) :: "cc"); // rdrand %rax
+        if (ok) break;
+    }
+    return r1;
+#else
+#error "RdRand is only supported on x86 and x86_64"
+#endif
+}
+
+
+int true = 1;
+
+static unsigned long long GetRdSeed()
+{
+    // RdSeed may fail when the HW RNG is overloaded. Loop indefinitely until enough entropy is gathered,
+    // but pause after every failure.
+#ifdef __i386__
+    unsigned char ok;
+    unsigned integer r1, r2;
+    do {
+        __asm__ volatile (".byte 0x0f, 0xc7, 0xf8; setc %1" : "=a"(r1), "=q"(ok) :: "cc"); // rdseed %eax
+        if (ok) break;
+        __asm__ volatile ("pause");
+    } while(true);
+    do {
+        __asm__ volatile (".byte 0x0f, 0xc7, 0xf8; setc %1" : "=a"(r2), "=q"(ok) :: "cc"); // rdseed %eax
+        if (ok) break;
+        __asm__ volatile ("pause");
+    } while(true);
+    return (((uint64_t)r2) << 32) | r1;
+#elif defined(__x86_64__) || defined(__amd64__)
+    unsigned char ok;
+    unsigned long long r1;
+    do {
+        __asm__ volatile (".byte 0x48, 0x0f, 0xc7, 0xf8; setc %1" : "=a"(r1), "=q"(ok) :: "cc"); // rdseed %rax
+        if (ok) break;
+        __asm__ volatile ("pause");
+    } while(true);
+    return r1;
+#else
+#error "RdSeed is only supported on x86 and x86_64"
+#endif
+}
+
 int
 main(void)
 {
@@ -36,6 +105,28 @@ main(void)
                 e = rdtsc();
                 d += e - b;
         }
-        printf("average : %.3f\n", (double)d / 1000000.0);
+        printf("rdtsc average : %.3f\n", (double)d / 1000000.0);
+
+        // rdrand
+        for (i = 0; i < 1000; i ++) {
+                unsigned long long b, e;
+
+                b = GetRdRand();
+                e = GetRdRand();
+                d += e - b;
+        }
+        printf("rdrand average : %.3f\n", (double)d / 1000.0);
+
+        // rdseed
+        for (i = 0; i < 1000; i ++) {
+                unsigned long long b, e;
+
+                b = GetRdSeed();
+                e = GetRdSeed();
+                d += e - b;
+        }
+        printf("rdseed average : %.3f\n", (double)d / 1000.0);
+
+
         return 0;
 }
